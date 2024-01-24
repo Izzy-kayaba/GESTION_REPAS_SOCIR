369151fd058e4eecad99219f700c8ff4bca4a032
izzy-kayaba <israelkayaba@gmail.com>
2024-01-24 13:24:19 +0200

    improved env file

--- server/config/database.js
+++ server/config/database.js
@@ -3,42 +3,42 @@ const path = require('path');
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
   const connections = {
-      postgres: {
-            connection: {
-              connectionString: env('DATABASE_URL'),
-              host: env('DATABASE_HOST', 'localhost'),
-              port: env.int('DATABASE_PORT', 5432),
-              database: env('DATABASE_NAME', 'testDB'),
-              user: env('DATABASE_USERNAME', 'postgres'),
-              password: env('DATABASE_PASSWORD', 'djuma2022'),
-              schema: env('DATABASE_SCHEMA', 'public'),
-              ssl: env.bool('DATABASE_SSL', false) && {
-                key: env('DATABASE_SSL_KEY', undefined),
-                cert: env('DATABASE_SSL_CERT', undefined),
-                ca: env('DATABASE_SSL_CA', undefined),
-                capath: env('DATABASE_SSL_CAPATH', undefined),
-                cipher: env('DATABASE_SSL_CIPHER', undefined),
-                rejectUnauthorized: env.bool(
-                  'DATABASE_SSL_REJECT_UNAUTHORIZED',
-                  true
-                ),
-              },
-            },
-            pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-            debug: true,
-          },
-       sqlite: {
-            connection: {
-              filename: path.join(
-                __dirname,
-                '..',
-                env('DATABASE_FILENAME', '.tmp/data.db')
-              ),
-            },
-            useNullAsDefault: true,
-          },
-        }
-      return {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+        user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+        schema: env('DATABASE_SCHEMA', process.env.DATABASE_SCHEMA),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
+        },
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      debug: true,
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', process.env.DATABASE_FILENAME)
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  }
+  return {
     connection: {
       client,
       ...connections[client],
